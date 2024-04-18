1bb0fa24a28b417cf7e74a57bdd59992e5011628
githukelvin <kelvingithu019@gmail.com>
2024-04-18 16:50:45 +0300

    started on dasboards privates ui

---
+++ src/layouts/MainMenuLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="grid grid-cols-[25vw_75vw]">
+    <SideNavbar/>
+    <div class="bg-black w-vw h-svh">
+      <RouterView/>
+    </div>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import SideNavbar from '@/components/SideNavbar.vue'
+</script>
+
+<style scoped>
+
+</style>
